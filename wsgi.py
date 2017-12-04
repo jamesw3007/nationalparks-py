18a18970863dc9d8c3e0c380abb51ae010b5df80
jamesw3007 <jamesw3007@gmail.com>
2017-12-04 16:42:43 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'National Parks (PY) ABC123',
+        'displayName': 'National Parks (PY) ABC123hello',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
